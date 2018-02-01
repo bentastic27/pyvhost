c91bb0fe6c51ffcd069735703d4f42d57765198a
Ben Healey <bentastic27@gmail.com>
2018-01-31 22:42:44 -0500

    Got the basics for django-bootstrap3 going

--- pyvhost/apache_config/views/index.py
+++ pyvhost/apache_config/views/index.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Apache config index")
+    return render(request, 'apache_config/index.html')
