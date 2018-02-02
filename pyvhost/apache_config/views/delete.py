5762713b5edb3da1f0137cb2af16779de3f74773
Ben Healey <bentastic27@gmail.com>
2018-02-01 19:10:09 -0500

    finished the delete view

--- pyvhost/apache_config/views/delete.py
+++ pyvhost/apache_config/views/delete.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
+from django.http import HttpResponseRedirect
 from ..models import VirtualHost
 from ..forms import DeleteConfirmation
 
 
 def delete(request, vhost_id):
     vhost = get_object_or_404(VirtualHost, pk=vhost_id)
-    confirm_delete = DeleteConfirmation
+    confirm_delete = DeleteConfirmation(request.POST or None)
+
+    if request.method == 'POST' and confirm_delete.is_valid():
+        if confirm_delete.cleaned_data['confirm']:
+            vhost.delete()
+            return HttpResponseRedirect(
+                reverse('apache_config:list')
+            )
+
     return render(
         request,
         'apache_config/delete.html',
