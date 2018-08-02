f87c72368b89afa17bf792bb1e40dd0810c2fab4
Ben Healey <bentastic27@gmail.com>
2018-08-02 13:46:24 -0400

    Got vhost generation working, needs further work with let's encrypt so it'll actually generate a working https vhost

--- pyvhost/apache_config/config_functions/apache_vhost.py
+++ pyvhost/apache_config/config_functions/apache_vhost.py
@@ -1,4 +1,4 @@
 from django.template.loader import render_to_string
 
 def gen_vhost(vhost):
-    return render_to_string("vhost.txt", {"vhost": vhost})
+    return render_to_string("apache_config/vhost.txt", {"vhost": vhost})
